62800844b64e2772e6e98c1ae9ff7bcf9dd1fb2c
PP <46500171+ZHUANGPP@users.noreply.github.com>
2022-07-06 14:41:48 -0700

    Update FlixerUITests.m

--- FlixerUITests/FlixerUITests.m
+++ FlixerUITests/FlixerUITests.m
@@ -15,11 +15,9 @@
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
-
     // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = NO;
-
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    self.continueAfterFailure = NO;
 }
 
 - (void)tearDown {
@@ -28,10 +26,9 @@
 
 - (void)testExample {
     // UI tests must launch the application that they test.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
     XCUIApplication *app = [[XCUIApplication alloc] init];
     [app launch];
-
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
 - (void)testLaunchPerformance {
